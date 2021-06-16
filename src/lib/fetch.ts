6483c3ced63acbc1787b520875e3098f8620b9c0
rewdaraw <rewdaraw@gmail.com>
2021-06-16 11:09:52 +0900

    create getAllPostsData, getAllPostIds func

---
+++ src/lib/fetch.ts
@@ -0,0 +1,13 @@
+import { useFetch } from '../hooks/useFetch'
+import { IPost } from '../types/types'
+
+export const getAllPostsData = async (): Promise<IPost[]> =>
+  await useFetch('get-blogs')
+
+export const getAllPostIds = async (): Promise<{ params: string }[]> => {
+  const posts: IPost[] = await useFetch('get-blogs')
+
+  return posts.map((post) => {
+    return { params: String(post.id) }
+  })
+}
