9ddacb9e4d1ce80219b80b6477e1cc5287555c48
rewdaraw <rewdaraw@gmail.com>
2021-06-16 13:08:02 +0900

    getStaticProps typing by self made type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,31 @@
-const BlogPage: React.FC = () => {
+import { GetStaticProps } from 'next'
+import { getAllPostsData } from '../lib/fetch'
+import { IPost } from '../types/types'
+
+interface IProps {
+  posts: IPost[]
+}
+
+const BlogPage: React.FC<IProps> = ({ posts }) => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen font-mono">
       Hello Nextjs
+      {posts &&
+        posts.map((post) => (
+          <p className="text-2xl" key={post.title}>
+            {post.title}
+          </p>
+        ))}
     </div>
   )
 }
 export default BlogPage
+
+export const getStaticProps: GetStaticProps<IProps> = async () => {
+  const posts = await getAllPostsData()
+
+  return {
+    props: { posts },
+    revalidate: 3,
+  }
+}
