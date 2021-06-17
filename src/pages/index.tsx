232152e7423e419cb1a21302f9768caa83b8827a
rewdaraw <rewdaraw@gmail.com>
2021-06-17 13:42:13 +0900

    create logout, deletePost func

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { InferGetStaticPropsType } from 'next'
 import { getAllPostsData } from '../lib/fetch'
 import Cookies from 'universal-cookie'
 import { useState } from 'react'
+import axios from 'axios'
 import { useEffect } from 'react'
 
 // MEMO: InferGetStaticPropsType<typeof getStaticProps>でpropsの型を類推できる
@@ -13,6 +14,34 @@ const cookie = new Cookies()
 const BlogPage: React.FC<IProps> = ({ posts }) => {
   // ページに訪れるユーザーがログイン済みか判定するフラグ
   const [hasToken, setHasToken] = useState(false)
+
+  const logout = () => {
+    cookie.remove('access_token')
+    setHasToken(false)
+  }
+
+  const deletePost = async (id: string) => {
+    const axiosInstance = axios.create({
+      baseURL: `${process.env.NEXT_PUBLIC_RESTAPI_URL}`,
+    })
+    axiosInstance.interceptors.request.use(
+      (config) => {
+        if (cookie.get('access_token')) {
+          config.headers.Authorization = `JWT ${cookie.get('access_token')}`
+          return config
+        }
+      },
+      function (error) {
+        return Promise.reject(error)
+      }
+    )
+    await axiosInstance
+      .delete(`${process.env.local}/delete-blog/${id}`)
+      .catch(() => {
+        throw new Error('削除リクエストに失敗しました')
+      })
+  }
+
   // cookieが存在するかしないかの判定
   useEffect(() => {
     if (cookie.get('access_token')) {
