2fae7adc971db417659ef4967cbd010510fbdf74
rewdaraw <rewdaraw@gmail.com>
2021-06-21 05:05:17 +0900

    change name useFetch to getData

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const useFetch = async (uri: string) => {
+export const getData = async (uri: string) => {
   return await axios
     .get(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/${uri}`)
     .then((response) => {
--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,13 +1,13 @@
-import { useFetch } from '../hooks/useFetch'
+import { getData } from '../hooks/useFetch'
 import { IPost } from '../types/types'
 
 export const getAllPostsData = async (): Promise<IPost[]> =>
-  await useFetch('get-blogs')
+  await getData('get-blogs')
 
 export const getAllPostIds = async (): Promise<
   { params: { [slug: string]: string } }[]
 > => {
-  const posts: IPost[] = await useFetch('get-blogs')
+  const posts: IPost[] = await getData('get-blogs')
 
   return posts.map((post) => {
     return { params: { id: String(post.id) } }
@@ -15,4 +15,4 @@ export const getAllPostIds = async (): Promise<
 }
 
 export const getPostData = async (id: string): Promise<IPost> =>
-  await useFetch(`get-blogs/${id}`)
+  await getData(`get-blogs/${id}`)
