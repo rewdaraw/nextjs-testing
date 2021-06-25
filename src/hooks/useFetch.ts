6c59e2d784007216bab626350b94c6974e5a10df
rewdaraw <rewdaraw@gmail.com>
2021-06-26 01:55:04 +0900

    fix server side fetch code

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,20 +1,43 @@
-import axios from 'axios'
-
-export const getData = async (uri: string) => {
-  return await axios
-    .get(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/${uri}`)
-    .then((response) => {
-      return response.data
-    })
-    .catch(() => {
-      throw new Error('データ取得に失敗しました')
-    })
-}
-
 export const postData = async (uri: string, data: any) => {
   return await axios
     .post(`${process.env.NEXT_PUBLIC_RESTAPI_URL}/${uri}`, data)
     .then((response) => {
       return response.data
     })
+}
+
+import axios, {
+  AxiosResponse,
+  AxiosError,
+  Method,
+} from 'axios'
+interface IUseAxiosParams {
+  url: string
+  method: Method
+  data?: {} | null
+  headers?: { [key: string]: string }
+}
+
+export const useServerAxios = async <T>({
+  url,
+  method,
+  data = null,
+  headers = null,
+}: IUseAxiosParams): Promise<AxiosResponse<T> | AxiosError<T>> => {
+  let axiosInstance = axios.create({
+    // baseURL: `https://jsonplaceholder.typicode.com`,
+    baseURL: `${process.env.NEXT_PUBLIC_RESTAPI_URL}`,
+    headers: {
+      'Content-Type': 'application/json',
+      headers,
+    },
+    timeout: 1000,
+  })
+
+  const res = await axiosInstance[method](url, data, headers)
+    .catch((error: AxiosError<T>) => {
+      return error
+    })
+
+  return res
 }
--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -1,18 +1,53 @@
-import { getData } from '../hooks/useFetch'
 import { IPost } from '../types/types'
+import { useServerAxios } from '../hooks/useFetch'
+import { AxiosResponse } from 'axios'
 
-export const getAllPostsData = async (): Promise<IPost[]> =>
-  await getData('get-blogs')
-
-export const getAllPostIds = async (): Promise<
-  { params: { [slug: string]: string } }[]
-> => {
-  const posts: IPost[] = await getData('get-blogs')
-
-  return posts.map((post) => {
-    return { params: { id: String(post.id) } }
+export const getAllPostsData = async () => {
+  const res = await useServerAxios<IPost[]>({
+    url: "get-blogs",
+    method: "get"
   })
+
+  // type guard
+  const isAllPostResponse = (res: any): res is AxiosResponse<IPost[]> => {
+    return res.data
+  }
+
+  if (isAllPostResponse(res)) {
+    return res.data
+  }
 }
 
-export const getPostData = async (id: string): Promise<IPost> =>
-  await getData(`get-blogs/${id}`)
+export const getAllPostIds = async () => {
+  const res = await useServerAxios<IPost[]>({
+    url: `get-blogs`,
+    method: 'get',
+  })
+
+  // type guard
+  const isAllPostResponse = (res: any): res is AxiosResponse<IPost[]> => {
+    return res.data
+  }
+
+  if (isAllPostResponse(res)) {
+    return res.data.map((post) => {
+      return { params: { id: String(post.id) } }
+    })
+  }
+}
+
+export const getPostData = async (id: string) => {
+  const res = await useServerAxios<IPost>({
+    url: `get-blogs/${id}`,
+    method: 'get',
+  })
+
+  // type guard
+  const isPostResponse = (res: any): res is AxiosResponse<IPost> => {
+    return res.data
+  }
+
+  if (isPostResponse(res)) {
+    return res.data
+  }
+}
